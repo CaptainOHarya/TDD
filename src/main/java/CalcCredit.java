c0da62535dc29c42b95509cf63f88a5570c44b35
Leonid Zulin <leo.zulin@gmail.com>
2021-12-06 20:43:45 +0300

    Разработка тестов

---
+++ src/main/java/CalcCredit.java
@@ -0,0 +1,24 @@
+public class CalcCredit {
+    public static void main(String[] args) {
+
+    }
+    // Напишем методы заглушки для будущих методов
+
+    // Рассчет месячного платежа
+    public double calcOfMounthlyPayment(int loanSum, double interestRate, int creditPeriod) {
+
+        return 0;
+    }
+
+    // метод расчёта общей суммы к возврату в банк
+    public double calcOfTotalSum(int loanSum, double interestRate){
+
+        return 0;
+    }
+
+    // метод расчёта переплаты за весь период
+    public double calcOfOverpayment (int loanSum, double interestRate) {
+
+        return 0;
+    }
+}
